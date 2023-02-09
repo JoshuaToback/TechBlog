be30a25a37e864d0ec332612927fdc06482c5bd9
JoshuaToback <joshuatoback@gmail.com>
2023-02-09 16:44:17 -0500

    Finalized site, debugged routing issues

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: 'secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -23,7 +23,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
